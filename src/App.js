6fe08525f547c70653127d3bae0a44065fbffd67
Frank Luongo <frank.luongo@revzilla.com>
2019-01-24 09:13:13 -0500

    3: How Styled Components Work

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 
 const Heading = styled.h1`
-  background-color: #eee;
+  margin: 0;
   padding: 15px;
+
+  font-size: 3rem;
+
+  background-color: #eee;
 `;
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Heading>
-          Heading
-        </Heading>
+        <Heading>Heading</Heading>
       </Fragment>
     );
   }
