067e29e001f8cc24cabb7f63a3a417185ea81c83
Frank Luongo <frank.luongo@revzilla.com>
2019-01-25 14:04:17 -0500

    6: Styling Components & Extending

--- src/App.js
+++ src/App.js
@@ -1,40 +1,5 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-
-const Heading = styled.h1`
-  margin: 0;
-  padding: 15px;
-
-  font-size: 3rem;
-`;
-
-const buttonColor = ({type}) => {
-  return type === 'cancel' ? 'tomato;' : '#fff';
-}
-
-const Button = styled.button`
-  padding: 15px 30px;
-
-  color: #333;
-  font-size: 1.2rem;
-  font-weight: bold;
-  text-transform: uppercase;
-
-  background-color: ${buttonColor.bind(this)};
-  border: 1px solid #eee;
-  border-radius: 4px;
-  cursor: pointer;
-`;
-
-const AppWrapper = styled.div`
-  header {
-    background: teal;
-
-    &:hover {
-      background: red;
-    }
-  }
-`;
+import { AppWrapper, Heading, Button, CancelButton, StyledFake, Fake } from './Styles/General';
 
 class App extends Component {
   render() {
@@ -42,7 +7,9 @@ class App extends Component {
       <AppWrapper>
         <header>Header</header>
         <Heading>Heading</Heading>
-        <Button type="cancel">Button</Button>
+        <StyledFake />
+        <Fake />
+        <CancelButton type="cancel">Button</CancelButton>
         <Button type="save">Button</Button>
       </AppWrapper>
     );
---
+++ src/Styles/General.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Fake = ({ className }) => <div className={className}><h2>Yo!</h2></div>;
+
+export const Heading = styled.h1`
+  margin: 0;
+  padding: 15px;
+
+  font-size: 3rem;
+`;
+
+export const Button = styled.button`
+  padding: 15px 30px;
+
+  color: #333;
+  font-size: 1.2rem;
+  font-weight: bold;
+  text-transform: uppercase;
+
+  background-color: white;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+`;
+
+export const CancelButton = styled(Button)`
+  background-color: tomato;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const AppWrapper = styled.div`
+  padding: 15px;
+
+  ${Button} {
+    margin-left: 20px;
+
+    &:first-of-type {
+      margin-left: 0;
+    }
+  }
+`;
+
+export const StyledFake = styled(Fake)` color: red;`;
