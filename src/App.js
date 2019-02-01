70f6ff6643c247b8878a1dbf61e4add33cc5c1ec
Frank Luongo <frank.luongo@revzilla.com>
2019-02-01 09:27:18 -0500

    19: Themes

--- src/App.js
+++ src/App.js
@@ -3,30 +3,35 @@ import { AppWrapper, Heading, Button, StyledFake, Fake } from './elements';
 import GlobalStyle from './elements/Global';
 import Header from './layouts/Header';
 import { Card } from './elements/Cards';
-// Install The Polish Library for color functions
-// npm install --save polished
-// Install the Modifiers Library
-// npm install --save styled-components-modifiers
+import { ThemeProvider } from 'styled-components';
 
+const theme = {
+  colors: {
+    red: '#e60000',
+    blue: '#3ec9ef',
+    black: '#212121',
+    white: '#f1f1f1'
+  }
+};
 
 class App extends Component {
   render() {
     return (
-      <>
-      <Header />
-      <AppWrapper>
-        <Heading>Heading</Heading>
-        <Card>
-          <p>This is a card</p>
-          <Card.Button modifiers={['typeStandard', 'sizeDefault']}>Button</Card.Button>
-        </Card>
-        <StyledFake />
-        <Fake />
-        <Button modifiers={['typeCancel', 'sizeDefault']} top="100">Button</Button>
-        <Button modifiers={['typeStandard', 'sizeSmall']}>Button</Button>
-        <GlobalStyle />
-      </AppWrapper>
-      </>
+        <>
+        <Header />
+        <AppWrapper>
+          <Heading>Heading</Heading>
+          <Card>
+            <p>This is a card</p>
+            <Card.Button modifiers={['typeStandard', 'sizeDefault']}>Button</Card.Button>
+          </Card>
+          <StyledFake />
+          <Fake />
+          <Button modifiers={['typeCancel', 'sizeDefault']} top="100">Button</Button>
+          <Button modifiers={['typeStandard', 'sizeSmall']}>Button</Button>
+          <GlobalStyle />
+        </AppWrapper>
+        </>
     );
   }
 }
